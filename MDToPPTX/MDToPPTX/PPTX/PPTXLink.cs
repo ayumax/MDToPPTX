90b64fc297fbab70917a956b0228267388a94a8d
Ayumax <ayuma0913@gmail.com>
2018-05-29 00:18:06 +0900

    ハイパーリンク実装開始

---
+++ MDToPPTX/MDToPPTX/PPTX/PPTXLink.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDToPPTX.PPTX
+{
+    public class PPTXLink
+    {
+        public string LinkKey { get; set; } = "";
+        public string LinkURL { get; set; } = "";
+    }
+}
